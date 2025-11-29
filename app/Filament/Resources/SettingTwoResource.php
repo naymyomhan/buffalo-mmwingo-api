403baa7d87f2de647c5d1ab1d1df1e43dd84487f
naymyomhan <naymyomhan@gmail.com>
2025-11-29 17:47:08 +0700

    updatje

--- app/Filament/Resources/SettingTwoResource.php
+++ app/Filament/Resources/SettingTwoResource.php
@@ -21,7 +21,7 @@ class SettingTwoResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    // protected static bool $shouldRegisterNavigation = false;
+    protected static bool $shouldRegisterNavigation = false;
 
     public static function form(Form $form): Form
     {
